40b8b02e7489e017ecd12028f487a36ff6fea175
Gustavo <gustavo.javax@gmail.com>
2025-09-25 18:40:25 -0300

    initial commit

---
+++ src/main/java/com/geradordenotas/gerador_de_notas/GeradorDeNotasApplication.java
@@ -0,0 +1,13 @@
+package com.geradordenotas.gerador_de_notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorDeNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geradordenotas/gerador_de_notas/GeradorDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geradordenotas.gerador_de_notas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
